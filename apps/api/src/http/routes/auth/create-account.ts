03f89c6954599dedc7d422e3541eea57af686b6b
jltrench <jltrench@gmail.com>
2024-05-26 11:03:18 -0300

    feat: swagger setup

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -12,6 +12,8 @@ export async function createAccount(app: FastifyInstance) {
     '/users',
     {
       schema: {
+        tags: ['auth'],
+        summary: 'Create a new account',
         body: z.object({
           name: z.string(),
           email: z.string().email(),
