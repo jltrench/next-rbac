6c9e9813f7dc653eb35cc30173b28ec911425628
jltrench <jltrench@gmail.com>
2024-07-14 01:05:56 -0300

    feat: billing info

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -14,6 +14,7 @@ import {
 import { errorHandler } from '@/http/error-handler'
 import { authenticateWithGithub } from '@/http/routes/auth/authenticate-with-github'
 import { authenticateWithPassword } from '@/http/routes/auth/authenticate-with-password'
+import { createAccount } from '@/http/routes/auth/create-account'
 import { getProfile } from '@/http/routes/auth/get-profile'
 import { requestPasswordRecover } from '@/http/routes/auth/request-password-recover'
 import { resetPassword } from '@/http/routes/auth/reset-password'
@@ -22,10 +23,25 @@ import { getMembership } from '@/http/routes/orgs/get-membership'
 import { getOrganization } from '@/http/routes/orgs/get-organization'
 import { getOrganizations } from '@/http/routes/orgs/get-organizations'
 import { shutdownOrganization } from '@/http/routes/orgs/shutdown-organization'
+import { transferOrganization } from '@/http/routes/orgs/transfer-organizations'
 import { updateOrganization } from '@/http/routes/orgs/update-organization'
+import { createProject } from '@/http/routes/projects/create-project'
+import { deleteProject } from '@/http/routes/projects/delete-project'
 
-import { createAccount } from './routes/auth/create-account'
-import { transferOrganization } from './routes/orgs/transfer-organizations'
+import { getOrganizationBilling } from './routes/billing/get-organization-billing'
+import { acceptInvite } from './routes/invites/accept-invite'
+import { createInvite } from './routes/invites/create-invite'
+import { getInvite } from './routes/invites/get-invite'
+import { getInvites } from './routes/invites/get-invites'
+import { getPendingInvites } from './routes/invites/get-pending-invites'
+import { rejectInvite } from './routes/invites/reject-invite'
+import { revokeInvite } from './routes/invites/revoke-invite'
+import { getMembers } from './routes/members/get-members'
+import { removeMember } from './routes/members/remove-member'
+import { updateMember } from './routes/members/update-member'
+import { getProject } from './routes/projects/get-project'
+import { getProjects } from './routes/projects/get-projects'
+import { updateProject } from './routes/projects/update-project'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -78,6 +94,26 @@ app.register(getOrganizations)
 app.register(updateOrganization)
 app.register(shutdownOrganization)
 app.register(transferOrganization)
+
+app.register(getProject)
+app.register(getProjects)
+app.register(createProject)
+app.register(updateProject)
+app.register(deleteProject)
+
+app.register(getMembers)
+app.register(updateMember)
+app.register(removeMember)
+
+app.register(createInvite)
+app.register(getInvite)
+app.register(getInvites)
+app.register(getPendingInvites)
+app.register(acceptInvite)
+app.register(rejectInvite)
+app.register(revokeInvite)
+
+app.register(getOrganizationBilling)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running!')
